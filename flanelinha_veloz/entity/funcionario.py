ca9e858f0bd43f4e91d4eaa4b4bf6e3db57489ed
Rafaela Tasso <atassorafaela@gmail.com>
2022-05-21 20:54:15 -0300

    remove line

--- flanelinha_veloz/entity/funcionario.py
+++ flanelinha_veloz/entity/funcionario.py
@@ -4,4 +4,3 @@ from datetime import datetime as dt, timedelta
 class Funcionario(UsuarioEmpresa):
     def __init__(self, cpf: str, data_nacimento: dt, email: str, genero: str, nome: str, senha: str, sobrenome: str, cargo: str, turno: list) -> None:
         super().__init__(cpf, data_nacimento, email, genero, nome, senha, sobrenome, cargo, turno)
-
