590c016a1c15b443ee5bc815a3d4cab01aaa21eb
Rafaela Tasso <atassorafaela@gmail.com>
2022-05-29 21:25:35 -0300

    finished create employees view and controler

--- flanelinha_veloz/control/employees_controller.py
+++ flanelinha_veloz/control/employees_controller.py
@@ -1,14 +1,15 @@
 from flanelinha_veloz.entity.funcionario import Funcionario
+from flanelinha_veloz.entity.gestor import Gestor
 from flanelinha_veloz.exceptions.cpfNotValid import CPFNotValid
 from flanelinha_veloz.exceptions.emailDoesntMatchException import EmailDoesntMatchException
+from flanelinha_veloz.exceptions.emailNotValid import EmailNotValid
 from flanelinha_veloz.exceptions.employeesNotWrited import EmployeesNotWrited
 from flanelinha_veloz.exceptions.passwordDoesntMatchException import PasswordDoesntMatchException
-from flanelinha_veloz.exceptions.userAlreadyExistException import UserAlreadyExistException
-from flanelinha_veloz.view.abstract_boundary import AbstractBoundary
 from flanelinha_veloz.view.employees_boundary import EmployeesBoundary
 from flanelinha_veloz.persistence.employeesDAO import EmployeesDAO
 from flanelinha_veloz.exceptions.employeesAlreadyExistsInTheSystem import EmployeesAlreadyExistsInTheSystem
-# from datetime import datetime as dt
+from datetime import datetime as dt
+from pprint import pprint
 
 class EmployeesController:
     def __init__(self, system_controller) -> None:
@@ -16,14 +17,26 @@ class EmployeesController:
         self.__boundary = EmployeesBoundary()
         self.__employee_dao = EmployeesDAO()
 
+    def open_edit_employees_screen(self):
+        # pprint(vars(self.__employee_dao.get(cpf)))
+        cpf = int('09074322980')
+        a = self.__employee_dao.get(cpf)
+        print(a.data_nascimento)
+
     def open_add_employees_screen(self):
         while True:
             try:
                 valores = self.__boundary.registration_employees_screen()
                 acao = valores['acao']
+                all_value_good = True
                 if acao == EmployeesBoundary.SUBMIT:
+                    for value in valores:
+                        if valores[value] is None or valores[value] == '': 
+                            all_value_good = False
                     cpf = valores['cpf']
-                    if cpf is None or cpf == '':
+                    if all_value_good == False:
+                        raise ValueError
+                    elif cpf is None or cpf == '':
                         raise EmployeesNotWrited
                     elif self.validate_cpf(cpf) == False:
                         raise CPFNotValid
@@ -34,14 +47,16 @@ class EmployeesController:
                         confirmar_email = valores['confirmar_email']
                         if email != confirmar_email:
                             raise EmailDoesntMatchException
+                        elif self.validate_email(email) == False:
+                            raise EmailNotValid
                         else:
                             senha = valores['senha']
                             confirmar_senha = valores['confirmar_senha']
                             if senha != confirmar_senha:
                                 raise PasswordDoesntMatchException
-                    nome = valores['nome']
-                    if nome is not None and nome != '':
-                        data_nascimento = valores['data_nascimento']
+                    if all_value_good: 
+                        nome = valores['nome']
+                        data_nascimento = dt.strptime(valores['data_nascimento'], "%d/%m/%Y")
                         cpf = int(cpf)
                         genero = valores['genero']
                         sobrenome = valores['sobrenome']
@@ -58,12 +73,9 @@ class EmployeesController:
                 else:
                     break
             except ValueError:
-                self.__boundary.show_message("* Confira  os valores *")
+                self.__boundary.show_message("Existem campos em branco, confira!")
             except Exception as e:
                 self.__boundary.show_message(str(e))
-
-    def retornar(self):
-        self.__system_controller.open_login_screen()
 
     def employee_resgistration(self, employee: Funcionario):
         if isinstance(employee, Funcionario) and employee is not None and \
@@ -88,19 +100,28 @@ class EmployeesController:
         if len(numbers) != 11 or len(set(numbers)) == 1:
             return False
 
-        # Validação do primeiro dígito verificador:
         sum_of_products = sum(a*b for a, b in zip(numbers[0:9], range(10, 1, -1)))
         expected_digit = (sum_of_products * 10 % 11) % 10
         if numbers[9] != expected_digit:
             return False
 
-        # Validação do segundo dígito verificador:
         sum_of_products = sum(a*b for a, b in zip(numbers[0:10], range(11, 1, -1)))
         expected_digit = (sum_of_products * 10 % 11) % 10
         if numbers[10] != expected_digit:
             return False
 
         return True
+
+    def validate_email(self, email: str) -> bool:
+        has_char_neededs = False
+        sum = 0
+        for letter in email:
+            if letter == '@' or letter == '.':
+                sum += 1
+            if sum == 2:
+                has_char_neededs = True
+
+        return has_char_neededs
 
     def exit(self):
         exit(0)
