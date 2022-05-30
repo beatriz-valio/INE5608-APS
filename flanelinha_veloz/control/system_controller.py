d531a9c92b17c059a8e24fd4e5b595562a7a0955
André J. Bez Birolo <andrecentrobrasileiro@outlook.com>
2022-05-29 23:04:09 -0300

    fixed imports

--- flanelinha_veloz/control/system_controller.py
+++ flanelinha_veloz/control/system_controller.py
@@ -3,41 +3,6 @@ from flanelinha_veloz.control.employees_controller import EmployeesController
 from flanelinha_veloz.control.login_controller import LoginController
 
 
-def shutdown():
-    exit(0)
-
-
-def validate_cpf(cpf: str) -> bool:
-    numbers = [int(digit) for digit in cpf]
-
-    if len(numbers) != 11 or len(set(numbers)) == 1:
-        return False
-
-    sum_of_products = sum(a * b for a, b in zip(numbers[0:9], range(10, 1, -1)))
-    expected_digit = (sum_of_products * 10 % 11) % 10
-    if numbers[9] != expected_digit:
-        return False
-
-    sum_of_products = sum(a * b for a, b in zip(numbers[0:10], range(11, 1, -1)))
-    expected_digit = (sum_of_products * 10 % 11) % 10
-    if numbers[10] != expected_digit:
-        return False
-
-    return True
-
-
-def validate_email(email: str) -> bool:
-    has_char_neededs = False
-    sum = 0
-    for letter in email:
-        if letter == '@' or letter == '.':
-            sum += 1
-        if sum == 2:
-            has_char_neededs = True
-
-    return has_char_neededs
-
-
 class SystemController:
 
     def __init__(self):
@@ -55,3 +20,35 @@ class SystemController:
 
     def open_login_screen(self):
         self.__login_controller.open_screen()
+
+    def shutdown(self):
+        exit(0)
+
+    def validate_cpf(self, cpf: str) -> bool:
+        numbers = [int(digit) for digit in cpf]
+
+        if len(numbers) != 11 or len(set(numbers)) == 1:
+            return False
+
+        sum_of_products = sum(a * b for a, b in zip(numbers[0:9], range(10, 1, -1)))
+        expected_digit = (sum_of_products * 10 % 11) % 10
+        if numbers[9] != expected_digit:
+            return False
+
+        sum_of_products = sum(a * b for a, b in zip(numbers[0:10], range(11, 1, -1)))
+        expected_digit = (sum_of_products * 10 % 11) % 10
+        if numbers[10] != expected_digit:
+            return False
+
+        return True
+
+    def validate_email(self, email: str) -> bool:
+        has_char_neededs = False
+        sum = 0
+        for letter in email:
+            if letter == '@' or letter == '.':
+                sum += 1
+            if sum == 2:
+                has_char_neededs = True
+
+        return has_char_neededs
